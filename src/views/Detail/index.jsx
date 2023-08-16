5386dd1af5ef89c194d3288aa830d4ad032ffe46
Jonathanpv99 <jonyvegapjv@gmail.com>
2023-08-16 14:30:11 -0600

    detail page

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,8 +1,63 @@
+import { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
+import styles from './Detail.module.css'
+import { format } from "date-fns"
+import { es } from 'date-fns/locale';
 const Detail = () => {
     const {eventId} = useParams();
+    const [eventData, setEventData] = useState({});
+    const [error,setError] = useState('');
+    const[isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchEventData = async () => {
+            try {
+                const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${eventId}.json?apikey=pEpqjNqYmT7qPol77twiqFOE6YCC4XVQ`);
+                const data = await response;
+                setEventData(data);
+                setIsLoading(false);
+                isLoading(false);
+            } catch (error) {
+                setEventData({});
+                setError(error);
+
+            }
+        };
+        fetchEventData();
+    },[]);
+
+    if(isLoading && Object.keys(eventData) ===0){
+        return <div>Cargando evento...</div>
+    }
+
+    if(Object.keys(error) > 0){
+        return <div>{error}</div>
+    }
     return(
-        <div>Detail</div>
+        <div className={styles.container}>
+            <div className={styles.mainInforContainer}>
+                <img src={eventData.imges?.[0].url}  alt="imagen Evento" className={styles.eventImage}/>
+                <h4 className={styles.eventName}>{eventData.name}</h4>
+                <p className={styles.inforParagraph}>{eventData.info}</p>
+                {eventData.dates.start.dateTime ? 
+                <p className={styles.date}>
+                    {format(new Date(eventData?.dates.start.dateTime), 'd LLLL yyyy H:mm',{ locate: es})}
+                </p>
+                : null}
+                
+            </div>
+            <div className={styles.seatInfoContainer}>
+                <h6 className={styles.seatTitle}>Mapa del evento</h6>
+                <img src={eventData.satmap?.staticUrl} alt="mapa del evento" />
+                <p className={styles.pleaceNote}>{eventData.pleaseNote}</p>
+                <p className={styles.priceRange}>
+                    Rangp de precios:
+                    {eventData.priceRanges?.[0].min}-{eventData.priceRanges?.[0].max}
+                    {eventData.priceRanges?.[0].currency}
+                </p>
+                <a href={eventData.url}>Ir por tus boletos</a>
+            </div>
+        </div>
     );
 }
 
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,9 +1,13 @@
 import { useState, useRef, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
+
 import Navbar from '../../components/Navbar';
 import Events from '../../components/Events';
 import useEventsData from "../../hooks/useEventsData";
+import styles from './Home.module.css'
+
 const Home = () => {
-  const { events, isLoading, error, fetchEvents } = useEventsData();
+  const { events, isLoading, error, fetchEvents, page } = useEventsData();
   const [searchTerm, setSearchTerm] = useState('');
   const containerRef = useRef();
   
@@ -23,11 +27,43 @@ if(isLoading){
     return 
 }
 
+const handlePageClick = ({ selected }) => {
+  fetchEvents(`&keyword=${searchTerm}&page=${selected}`)
+}
+
+const renderEvents = () => {
+  if(isLoading){
+    return <div>Cargando Resultados...</div>;
+  }
+  if(error){
+    return <div> Ha ocurrido un error</div>;
+  }
+
+  return (
+    <div>
+    <Events searchTerm={searchTerm} events={events}/>
+    <ReactPaginate
+      className={styles.pagination}
+      nextClassName={styles.next}
+      previousClassName={styles.previous}
+      pageClassName={styles.page}
+      activeClassName={styles.activePage}
+      disabledClassName={styles.disablePage}
+      breakLabel="..."
+      nextLabel="next >"
+      onPageChange={handlePageClick}
+      pageRangeDisplayed={5}
+      pageCount={page.totalPages}
+      previousLabel="< previous"
+      renderOnZeroPageCount={null}
+    />
+    </div>
+    )
+}
   return (
     <>
       <Navbar onSearch={handleNavbarSearch} ref={containerRef}/>
-      {isLoading ? <div>Cargando Resultados...</div> : <Events searchTerm={searchTerm} events={events}/>}
-      {!!error && <div> Ha ocurrido un error</div>}
+      {renderEvents()}
     </>
   )
 }
